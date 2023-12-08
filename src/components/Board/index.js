a9f4733af6a6489f13a8e7846cd665b53feb30e1
vishal yadav <vishal7738639800@gmail.com>
2023-12-08 10:08:23 +0530

    Add ability to download

---
+++ src/components/Board/index.js
@@ -0,0 +1,107 @@
+import { useEffect, useLayoutEffect, useRef } from "react";
+import styles from "./index.module.css";
+import { useDispatch, useSelector } from "react-redux";
+import { MENU_ITEMS } from "@/constants";
+import { menuItemClick, actionItemClick } from "@/slice/menuSlice";
+const Board = () => {
+  const dispatch = useDispatch();
+  const canvasRef = useRef(null);
+  const shouldDraw = useRef(false);
+
+  const { activeMenuItem, actionMenuItem } = useSelector((state) => state.menu);
+  const { color, size } = useSelector((state) => state.toolbox[activeMenuItem]);
+
+  useEffect(() => {
+    if (!canvasRef.current) return;
+
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    if (actionMenuItem == MENU_ITEMS.DOWNLOAD) {
+      const URL = canvas.toDataURL();
+      const anchor = document.createElement("a");
+      anchor.href = URL;
+      anchor.download = "sketch.jpg";
+      anchor.click();
+    }
+
+    dispatch(actionItemClick(null));
+  }, [actionMenuItem, dispatch]);
+
+  useEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    const changeConfig = () => {
+      context.strokeStyle = color;
+      context.lineWidth = size;
+    };
+
+    changeConfig();
+  }, [color, size]);
+
+  // before browser paint
+  useLayoutEffect(() => {
+    if (!canvasRef.current) {
+      return;
+    }
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    const beginPath = (x, y) => {
+      context.beginPath();
+      context.moveTo(x, y);
+    };
+
+    const drawLine = (x, y) => {
+      context.lineTo(x, y);
+      context.stroke();
+    };
+
+    const handleMouseUp = (e) => {
+      shouldDraw.current = false;
+    };
+
+    const handleMouseMove = (e) => {
+      // If u don't have the permission to draw, then just return
+      if (!shouldDraw.current) {
+        return;
+      }
+
+      drawLine(e.clientX, e.clientY);
+    };
+
+    const handleMouseDown = (e) => {
+      shouldDraw.current = true;
+      context.lineJoin = "round";
+      context.lineCap = "round";
+      beginPath(e.clientX, e.clientY);
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, []);
+
+  console.log(color, size);
+
+  return (
+    <div>
+      <canvas ref={canvasRef}></canvas>
+    </div>
+  );
+};
+
+export default Board;
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import Board from "@/components/Board";
+import Menu from "@/components/Menu";
+import Toolbox from "@/components/Toolbox";
+
+const Home = () => {
+  return (
+    <>
+      <Menu />
+      <Toolbox />
+      <Board />
+    </>
+  );
+};
+
+export default Home;
